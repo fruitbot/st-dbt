3d1d4c5a450472facf63ff44215955c67ad56aa1
Shuting Zhang <shutingzhang@ShutingZhangs-MacBook-Pro.local>
2024-09-09 10:44:34 +0200

    Added sample incremental model

--- st_dbt_project/models/staging/stg_order_details.sql
+++ st_dbt_project/models/staging/stg_order_details.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key = 'order_details_id'
+    )
+}}
+
 with source as (
     select * from {{ source('source_kering_onboarding', 'order_details') }}
 ),
@@ -8,8 +15,12 @@ transformed as (
         OrderID as order_id,
         ProductID as product_id,
         Quantity as order_details_quantity,
-        current_timestamp() as ingested_at
+        cast('2024-09-09 08:06:05.275569 UTC' as timestamp) as ingested_at,
+        cast('2024-09-09 10:00:00.275569 UTC' as timestamp) as max_ingested_at
     from source
 )
 
 select * from transformed
+{% if is_incremental() %}
+where ingested_at >= (select max(max_ingested_at) from {{ this }})
+{% endif %}
